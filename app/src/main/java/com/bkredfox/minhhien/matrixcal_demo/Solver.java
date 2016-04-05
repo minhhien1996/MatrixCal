d0914074842455faa28461239020a303ff9890a0
minhhien1996 <minhhien139@gmail.com>
2016-04-05 13:55:10 +0700

    Second stable v

--- app/src/main/java/com/bkredfox/minhhien/matrixcal_demo/Solver.java
+++ app/src/main/java/com/bkredfox/minhhien/matrixcal_demo/Solver.java
@@ -1,10 +1,4 @@
-package com.bkredfox.minhhien.matrixcal_demo;
-
-/**
- * Created by minhhien on 13/03/2016.
- */
-
-public class Solver {
+public class Solve {
     public Matrix Add(Matrix m1, Matrix m2) {
         Matrix output = new Matrix(m1.rows, m1.cols);
         if (m1.DimMatch(m2)) {
@@ -13,8 +7,7 @@ public class Solver {
                     output.data[i][j] = m1.data[i][j] + m2.data[i][j];
                 }
         } else {
-            //System.out.print("Size Mismatch !");
-            //TODO: message for size mismatch
+            System.out.print("Size Mismatch !");
             return null;
         }
 
@@ -29,8 +22,7 @@ public class Solver {
                     output.data[i][j] = m1.data[i][j] - m2.data[i][j];
                 }
         } else {
-            //System.out.print("Size Mismatch !");
-            //TODO: message for size mismatch
+            System.out.print("Size Mismatch !");
             return null;
         }
 
@@ -85,28 +77,50 @@ public class Solver {
     public double Det(Matrix m) {
         if (m.rows != m.cols) return 0;
         if (m.rows == 1) return m.data[0][0];
-        //int sign = 1;
+        int sign = 1;
         double output = 0;
         if (m.rows == 2) output = m.data[0][0] * m.data[1][1] - m.data[0][1] * m.data[1][0];
         else for (int k = 0; k < m.rows; k++) {
-            //sign = (k+1)%2 == 0? 1: -1;
-            output += Det(m.Copy(1, (k + 1) % m.rows, m.rows - 1, m.rows - 1)) * m.data[0][k];
+            output += Det(m.Copy(1, (k + 1) % m.rows, m.rows - 1, m.rows - 1)) * m.data[0][k] * sign;
+            if (m.rows % 2 == 0) sign = -sign;
         }
         return output;
     }
 
     public Matrix Reverse(Matrix m) {
         Matrix output = new Matrix(m.cols, m.rows);
+        int sign = 1;
         for (int i = 0; i < m.rows; i++)
-            for (int j = 0; j < m.cols; j++)
-                output.data[j][i] = Det(m.Copy((i + 1) % m.rows, (j + 1) % m.cols, m.rows - 1, m.cols - 1));
-        double d = Det(m);
-        if (d != 0)
-            output = Multiply(output, 1 / d);
-        else {
-            //TODO
-        }
+            for (int j = 0; j < m.cols; j++) {
+                if (m.rows % 2 == 0) sign = ((i + j) % 2 == 0) ? 1 : -1;
+                output.data[j][i] = Det(m.Copy((i + 1) % m.rows, (j + 1) % m.cols, m.rows - 1, m.cols - 1)) * sign;
+
+            }
+        output = Multiply(output, 1 / Det(m));
         return output;
     }
 
+    Polynomial SigPoly(Matrix m) {
+        if (m.rows != m.cols) return null;
+        int n = m.rows;
+        Polynomial output = new Polynomial(n);
+        output.coef[n] = 1;
+        int sign = -1;
+        for (int k = 1; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+//				Matrix temp = m.Copy(i,i,k,k);
+                output.coef[n - k] += Det(m.Copy(i, i, k, k));
+            }
+            if (output.coef[n - k] != 0) output.coef[n - k] *= sign;
+            sign = -sign;
+        }
+        output.coef[0] = sign * Det(m);
+        return output;
+    }
+
+    public Roots Eigenvalues(Matrix m) {
+        Polynomial p = SigPoly(m);
+        Roots output = p.FindRoots();
+        return output;
+    }
 }
